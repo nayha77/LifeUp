4df148c83fb3d5ac52e058a3b136a1259b95230b
admin <admin@admin-PC>
2014-01-30 03:28:58 +0900

    ..

--- LifeUp/src/main/java/salesman/estimate/web/ContractController.java
+++ LifeUp/src/main/java/salesman/estimate/web/ContractController.java
@@ -1,15 +1,28 @@
 package salesman.estimate.web;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/contract/*")
 public class ContractController {
-	    
+	 
+	/*
+	 * 견적서 요청에 대한 견적 작성 폼
+	 */
     @RequestMapping("/write")
-    public String write() 
+    public String write(@RequestParam Map<String,Object> paramMap, ModelMap model, HttpServletRequest request)
     {
-    	return "estimate/contract/contractList";
+    	// 1. 파라메터 수신
+    	
+    	// 2. 견적요청 내용 가져오기    	
+    	
+    	return "estimate/contract/contractForm";
     }    
 }
---
+++ LifeUp/src/main/webapp/WEB-INF/views/estimate/contract/contractForm.jsp
@@ -0,0 +1,8 @@
+<%@ taglib prefix="mvc" tagdir="/WEB-INF/tags" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<mvc:layout>
+<script type="text/javascript">
+
+</script>
+</mvc:layout>
