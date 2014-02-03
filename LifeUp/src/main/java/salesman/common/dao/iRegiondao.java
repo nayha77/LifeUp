1c733a07244c60f3db90b3d75d090f1da833754c
Administrator <Administrator@WIN-I4NEFJ4JRPR>
2014-02-03 17:29:32 +0900

    여러가지 커밋

---
+++ LifeUp/src/main/java/salesman/common/dao/iRegiondao.java
@@ -0,0 +1,23 @@
+package salesman.common.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import salesman.model.Post;
+
+public interface iRegiondao {
+	
+	public List<HashMap<String,Object>> selectTestTable();
+	
+	public int writeProc(Post post);
+	
+	//public String getView(String postNo);
+	 public Post getBoard(int num); //읽기
+
+	public Post editing(Post post);
+
+	public int erase(Integer postNo);
+	
+
+}
---
+++ LifeUp/src/main/java/salesman/common/service/iRegionSelect.java
@@ -0,0 +1,12 @@
+package salesman.common.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface iRegionSelect {
+	
+	public List<HashMap<String, Object>> selectRegionSidoTable();
+	
+	public List<HashMap<String, Object>> selectRegionGuTable();
+	
+}
