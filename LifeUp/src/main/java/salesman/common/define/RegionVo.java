ed5d59e7a62ad53145eeb68ce118bc447919584d
Administrator <Administrator@WIN-I4NEFJ4JRPR>
2014-02-04 11:20:06 +0900

    ?

---
+++ LifeUp/src/main/java/salesman/common/define/RegionVo.java
@@ -0,0 +1,17 @@
+package salesman.common.define;
+
+public class RegionVo {
+	
+	String sido;
+
+	public String getSido() {
+		return sido;
+	}
+
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+	
+	
+
+}
