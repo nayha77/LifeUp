d206019dc1da73b467bc1c46034c8f9f262fea9e
Administrator <Administrator@WIN-I4NEFJ4JRPR>
2014-02-06 10:54:50 +0900

    tt

--- LifeUp/src/main/java/salesman/estimate/web/RequestController.java
+++ LifeUp/src/main/java/salesman/estimate/web/RequestController.java
@@ -1,7 +1,9 @@
 package salesman.estimate.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/request/*")
@@ -11,5 +13,20 @@ public class RequestController {
     public String list() 
     {
     	return "estimate/request/requestList";
+    }    
+    
+    @RequestMapping(value="/write", method=RequestMethod.GET)
+    public String write(
+            Integer articleNo, 
+            String boardCd, 
+            Integer curPage, 
+            String searchWord, 
+            Model model) throws Exception {
+         
+        //게시판 이름
+        String boardNm = boardService.getBoardNm(boardCd);
+        model.addAttribute("boardNm", boardNm);
+         
+        return "bbs/writeform";
     }    
 }
