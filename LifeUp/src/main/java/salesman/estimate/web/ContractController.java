76f25e156c425c0cc60c332a36e69e8a95df0fc0
admin <admin@admin-PC>
2014-02-17 01:09:08 +0900

    ..

--- LifeUp/src/main/java/salesman/estimate/web/ContractController.java
+++ LifeUp/src/main/java/salesman/estimate/web/ContractController.java
@@ -1,23 +1,39 @@
 package salesman.estimate.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import salesman.common.support.CustomException;
+import salesman.estimate.service.RequestService;
 
 @Controller
 @RequestMapping("/contract/*")
 public class ContractController {
 	 
-	/*
-	 * 견적서 요청에 대한 견적 작성 폼
-	 */
-    @RequestMapping("/writeform")
-//    public String write(@RequestParam int requestId)
-    public String write()
+	@Autowired
+	private RequestService requestService;
+	
+	@RequestMapping("/writeform")
+    public String write(@RequestParam(value="requestId", required=false) String requestId, ModelMap model)
     {
-    	// 1. 파라메터 수신
-    	
-    	// 2. 견적요청 내용 가져오기    	
-    	
+		int id = 0;
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		requestId = "4";
+				
+		if(requestId == null || requestId == "")
+			throw new CustomException("유효하지 않은 정보로 인해 페이지를 열 수 없습니다");
+			
+		id = Integer.parseInt(requestId);  
+		result = requestService.EstimateDetail(id);		
+		model.put("requestDetail", result);		
+		
     	return "estimate/contract/contractForm";
     }    
 }
